40a0c54ea2881dd8369f3cc604eed02903ad7679
surimLee <tnfla9689@naver.com>
2020-07-20 16:05:48 +0900

    Add Fragment.xml Edit Fragment.java

--- app/src/main/java/kr/co/waytech/peterparker/fragment/ParkingFragment.java
+++ app/src/main/java/kr/co/waytech/peterparker/fragment/ParkingFragment.java
@@ -1,6 +1,22 @@
 package kr.co.waytech.peterparker.fragment;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import kr.co.waytech.peterparker.R;
 
 public class ParkingFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_parking, container,false);
+        return view;
+    }
 }
