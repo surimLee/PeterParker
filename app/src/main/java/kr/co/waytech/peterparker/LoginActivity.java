ac3e42f8e3f3242fea5b87c3c89cc671e07ab91e
GiYeongUM <66294595+GiYeongUM@users.noreply.github.com>
2020-07-24 11:54:54 +0900

    commit Login/Signup ver 0.20
    2020-07-23
    로그인 회원가입 서버 통신 기능 구현

--- app/src/main/java/kr/co/waytech/peterparker/LoginActivity.java
+++ app/src/main/java/kr/co/waytech/peterparker/LoginActivity.java
@@ -1,27 +1,121 @@
 package kr.co.waytech.peterparker;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
+import java.io.IOException;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+import okhttp3.Call;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
-    Button signbtn;
+
+public class LoginActivity extends AppCompatActivity {
+    Button LoginBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        LoginBtn =(Button)findViewById(R.id.loginActivity_button_login);
+        LoginBtn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick (View v){
+                new Thread() {
+                    public void run() {
+                        Bundle bun = new Bundle();
+                        Message msg = handler.obtainMessage();
+                        msg.setData(bun);
+                        handler.sendMessage(msg);
+                        try {
+                            send_select();
+                            //send_login("eomky2005", "testtest");
+                            /*
+                            send_signup( "Email@naver.com", "eomky2005", "NameName", "testtest",
+                                    "NICKNICK", "01048136286", "123허1234");
 
-        signbtn = (Button)findViewById(R.id.loginActivity_button_signup);
-        signbtn.setOnClickListener(new View.OnClickListener() {    //led 액티비티
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+                             */
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
             }
         });
     }
+    Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            Bundle bun = msg.getData();
+            String serverHtml = bun.getString("Server_HTML");
+        }
+    };
+
+    public void send_login(String ID, String Password) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        MediaType mediaType = MediaType.parse("text/plain");
+        RequestBody body = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("user_id", ID)
+                .addFormDataPart("password", Password)
+                .build();
+        Request request = new Request.Builder()
+                .url("http://blazingcode.asuscomm.com/api/login")
+                .method("POST", body)
+                .build();
+        Response response = client.newCall(request).execute();
+        System.out.println("Response Body is " + response.body().string());
+    }
+
+    public void send_signup(String Email, String ID, String Name, String Password, String Nick_Name, String Phone, String Car) throws IOException {
+        try {
+            OkHttpClient client = new OkHttpClient().newBuilder().build();
+            MediaType mediaType = MediaType.parse("text/plain");
+            RequestBody requestBody = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("email", Email)
+                    .addFormDataPart("user_id", ID)
+                    .addFormDataPart("name", Name)
+                    .addFormDataPart("password", Password)
+                    .addFormDataPart("nick_name", Nick_Name)
+                    .addFormDataPart("phone_number", Phone)
+                    .addFormDataPart("car_number", Car)
+                    .build();
+            Request request = new Request.Builder()
+                    .url("http://blazingcode.asuscomm.com/api/register")
+                    .post(requestBody)
+                    .build();
+            Call call = client.newCall(request);
+            Response response = call.execute();
+            System.out.println("Response Body is " + response.body().string());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void send_select () throws IOException{
+        String token = "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9ibGF6aW5nY29kZS5hc3VzY29tbS5jb21cL2F" +
+                "waVwvbG9naW4iLCJpYXQiOjE1OTU0ODA2OTQsImV4cCI6MTU5NTQ4NDI5NCwibmJmIjoxNTk1NDgwNjk0LCJqdGkiOiJMMWhOeUl" +
+                "1ZGtxWUdyb2dDIiwic3ViIjoxOCwicHJ2IjoiODdlMGFmMWVmOWZkMTU4MTJmZGVjOTcxNTNhMTRlMGIwNDc1NDZhYSJ9.LnlZnm587" +
+                "6Oc4lEOsvZ8c2Sutn8nVbbKZs5cJSvrPLA";
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        Request request = new Request.Builder()
+                .url("http://blazingcode.asuscomm.com/")
+                .method("GET", null)
+                .addHeader("Authorization", token)
+                .build();
+        Response response = client.newCall(request).execute();
+        System.out.println("Response Body is " + response.body().string());
+    }
 }
+
+
+
+
--- app/src/main/java/kr/co/waytech/peterparker/PostClass.java
+++ app/src/main/java/kr/co/waytech/peterparker/PostClass.java
@@ -1,33 +1,25 @@
 package kr.co.waytech.peterparker;
+import android.os.Bundle;
+import android.os.Message;
+
 import java.io.IOException;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class PostClass {
-    public static final MediaType JSON
-            = MediaType.parse("application/json; charset=utf-8");
-
-    OkHttpClient client = new OkHttpClient();
-
-    String post(String url, String json) throws IOException{
-        RequestBody body = RequestBody.create(JSON, json);
-        Request request = new Request.Builder()
-                .url(url)
-                .post(body)
-                .build();
-        Response response = client.newCall(request).execute();
-        return response.body().string();
-    }
-    String LoginJson(String email, String name, String password){
-        return "'email'" + email + "'name'" + name + "'password'" + password;
-    }
-    public static void main(String[] args) throws  IOException{
-        PostClass postc = new PostClass();
-        String json = postc.LoginJson("emailemail", "nameaname", "passwordpasword");
-        String response = postc.post("blazingcode.asuscomm.com/api/register", json);
-        System.out.println(response);
-    }
+    /*
+ new void Thread() {
+        public void run() {
+            Bundle bun = new Bundle();
+            Message msg = handler.obtainMessage();
+            msg.setData(bun);
+            handler.sendMessage(msg);
+            try {
+                send_Login( "Email@naver.com", "eomky2005", "NameName", "testtest",
+                        "NICKNICK", "01048136286", "123허1234");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }.start();
+}
+*/
 }
