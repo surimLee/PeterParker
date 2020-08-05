f7f25390ad93690ae130765781ed4ab4d8345fc4
surimLee <tnfla9689@naver.com>
2020-08-05 13:39:58 +0900

    Revert "Add Booking List item Edit fontfamily"
    This reverts commit 47e2962d

--- app/src/main/java/kr/co/waytech/peterparker/fragment/BookingListFragment.java
+++ app/src/main/java/kr/co/waytech/peterparker/fragment/BookingListFragment.java
@@ -32,8 +32,7 @@ public class BookingListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//        View view = inflater.inflate(R.layout.fragment_bookinglist, container,false);
-        View view = inflater.inflate(R.layout.booking_list_item, container,false);
+        View view = inflater.inflate(R.layout.fragment_bookinglist, container,false);
 
         return view;
     }
